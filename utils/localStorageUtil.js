91692185b30bc2487be9805023a2f2d61cec2ab4
Dmitriy Pankin <pankindmp@ya.ru>
2023-05-24 02:12:36 +0800

    try4

--- utils/localStorageUtil.js
+++ utils/localStorageUtil.js
@@ -84,16 +84,14 @@ export class LocalStorageUtil {
 
     getTime(id) {
         let products = this.getProducts();
-        products.forEach(elem => {
-            let checkLs = document.createElement('div')
-            checkLs.innerHTML = JSON.parse(elem).time ;
-            document.body.append(checkLs) 
-        })
+       
         let f = products.find(element => JSON.parse(element).id === id);
         let dateStr = JSON.parse(f).time;
         dateStr = dateStr.slice(3,6) +  dateStr.slice(0, 2) + dateStr.slice(5);
         dateStr.slice(0, 2)
-        
+        let checkLs = document.createElement('div')
+            checkLs.innerHTML = dateStr;
+            document.body.append(checkLs, typeof dateStr) 
         return dateStr; //new Date('20.05.23')
     }
 
